bd0966b2b11c02320b1b1fc742abe08fe1a6f239
luzzif <fedeluzzi00@gmail.com>
2020-10-19 14:20:35 +0200

    feat: add token-list to dist folder

---
+++ dist/entities/token-list.d.ts
@@ -0,0 +1,18 @@
+export interface TokenInfo {
+    readonly chainId: number;
+    readonly address: string;
+    readonly name: string;
+    readonly decimals: number;
+    readonly symbol: string;
+    readonly logoURI?: string;
+}
+export interface Version {
+    readonly major: number;
+    readonly minor: number;
+    readonly patch: number;
+}
+export interface TokenList {
+    readonly name: string;
+    readonly tokens: TokenInfo[];
+    readonly logoURI?: string;
+}
