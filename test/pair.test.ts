2625e0e036c3c784679997c36358982b1147e6f6
Federico Luzzi <fedeluzzi00@gmail.com>
2021-03-02 00:02:46 +0100

    test: rename test

--- test/pair.test.ts
+++ test/pair.test.ts
@@ -21,7 +21,7 @@ describe('Pair', () => {
     })
   })
 
-  describe('#getIncorrectAddressFromWrongPlatform', () => {
+  describe('#getAddressfromWrongPlatform', () => {
     it('returns the incorrect address', () => {
       const usdc = new Token(ChainId.MAINNET, '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', 18, 'USDC', 'USD Coin')
       const dai = new Token(ChainId.MAINNET, '0x6B175474E89094C44Da98b954EedeAC495271d0F', 18, 'DAI', 'DAI Stablecoin')
