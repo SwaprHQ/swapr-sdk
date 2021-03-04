1c07c1ba71544f6fe767180e5591ff4841245b4d
Federico Luzzi <fedeluzzi00@gmail.com>
2021-03-04 21:30:58 +0100

    feat: use dxswap token registry id on xdai

--- src/constants.ts
+++ src/constants.ts
@@ -79,7 +79,7 @@ export const DXSWAP_TOKEN_LIST_ID: { [chainId: number]: number } = {
   [ChainId.RINKEBY]: 1,
   [ChainId.ARBITRUM_TESTNET_V3]: 1,
   [ChainId.SOKOL]: 1,
-  [ChainId.XDAI]: 1
+  [ChainId.XDAI]: 5
 }
 
 export const INIT_CODE_HASH = '0xd306a548755b9295ee49cc729e13ca4a45e00199bbd890fa146da43a50571776'
