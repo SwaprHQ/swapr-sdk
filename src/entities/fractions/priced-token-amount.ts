207165d84465b153b9cdc2e466444ce514a9482d
Federico Luzzi <fedeluzzi00@gmail.com>
2021-04-26 21:03:08 +0200

    fix: fix native currency amount calculation in priced token amount

--- src/entities/fractions/priced-token-amount.ts
+++ src/entities/fractions/priced-token-amount.ts
@@ -2,6 +2,7 @@ import { BigintIsh } from '../../constants'
 import { PricedToken } from '../priced-token'
 import { CurrencyAmount } from './currencyAmount'
 import { TokenAmount } from './tokenAmount'
+import { utils } from 'ethers'
 
 export class PricedTokenAmount extends TokenAmount {
   public readonly token: PricedToken
@@ -13,6 +14,14 @@ export class PricedTokenAmount extends TokenAmount {
   }
 
   public get nativeCurrencyAmount(): CurrencyAmount {
-    return new CurrencyAmount(this.token.price.quoteCurrency, this.raw)
+    return new CurrencyAmount(
+      this.token.price.quoteCurrency,
+      utils
+        .parseUnits(
+          this.multiply(this.token.price).toFixed(this.token.price.quoteCurrency.decimals),
+          this.token.price.quoteCurrency.decimals
+        )
+        .toString()
+    )
   }
 }
