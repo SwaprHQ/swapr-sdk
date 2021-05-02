f783af16c3e61f5ee286afcddb3e1c8c4d35b88b
Daniel Vega <dvega@tangocode.com>
2021-05-02 14:25:16 -0300

    adding matic values

--- src/entities/token.ts
+++ src/entities/token.ts
@@ -76,6 +76,13 @@ export class Token extends Currency {
       18,
       'DXD',
       'DXdao from Ethereum'
+    ),
+    [ChainId.MATIC]: new Token(
+      ChainId.MATIC,
+      '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270',
+      18,
+      'DXD',
+      'DXdao from Ethereum'
     )
   }
 
