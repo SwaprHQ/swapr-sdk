f9baf1b47e5259d0a5d962f8aa615df4197ceceb
Adam Azad <adam@adamazad.com>
2021-12-31 16:09:22 +0200

    feat(Cruve): add Arbitrum One support

--- src/entities/trades/curve-trade/CurveTrade.ts
+++ src/entities/trades/curve-trade/CurveTrade.ts
@@ -5,7 +5,7 @@ import { Contract } from '@ethersproject/contracts'
 import invariant from 'tiny-invariant'
 
 import { RoutablePlatform } from '../routable-platform/routable-platform'
-import { ChainId, ONE, TradeType, ZERO_ADDRESS } from '../../../constants'
+import { ChainId, ONE, TradeType } from '../../../constants'
 import { CurrencyAmount } from '../../fractions/currencyAmount'
 import { TokenAmount } from '../../fractions/tokenAmount'
 import { Fraction } from '../../fractions/fraction'
@@ -17,7 +17,7 @@ import { Currency } from '../../currency'
 
 // Curve imports
 import { getCurveContracts, getProvider, mapTokenSymbolToAddress } from './contracts'
-import { POOLS_XDAI } from './constants'
+import { CURVE_POOLS } from './constants'
 
 const ZERO_HEX = '0x0'
 
@@ -105,12 +105,6 @@ export class CurveTrade extends Trade {
         .invert()
         .multiply(this.outputAmount.raw).quotient
 
-      console.log({
-        slippageAdjustedAmountOut: slippageAdjustedAmountOut.toString(),
-        maximumSlippage: this.maximumSlippage,
-        outputAmount: this.outputAmount
-      })
-
       return this.outputAmount instanceof TokenAmount
         ? new TokenAmount(this.outputAmount.token, slippageAdjustedAmountOut)
         : CurrencyAmount.nativeCurrency(slippageAdjustedAmountOut, this.chainId)
@@ -173,19 +167,12 @@ export class CurveTrade extends Trade {
       const tokenInSymbol = currencyAmountIn.currency.symbol as string
       const tokenOutSymbol = currencyOut.symbol as string
       // Get the Router contract to populate the unsigned transaction
-      const { router: routerContract } = await getCurveContracts(chainId)
-      // Map symbols to address
-      const currencyInAddress = mapTokenSymbolToAddress(tokenInSymbol, chainId)
-      const currencyOutAddress = mapTokenSymbolToAddress(tokenOutSymbol, chainId)
-
+      // Get all Curve pools for the chain
+      const curvePools = CURVE_POOLS[chainId]
+      // Gnosis Chain / xDAI
       if (chainId == ChainId.XDAI) {
-        /**
-         * Gnosis Chain / xDAI
-         */
-        /**
-         * Curve's 3pool: WXDAI+USDC+USDT
-         */
-        const curve3Pool = POOLS_XDAI[0]
+        // Curve's 3pool: WXDAI+USDC+USDT
+        const curve3Pool = curvePools[0]
         const curve3PoolContract = new Contract(curve3Pool.swapAddress, curve3Pool.abi, getProvider(chainId))
         // Map token address to index
         const tokenInIndex = curve3Pool.tokens.findIndex(({ symbol }) => symbol == tokenInSymbol)
@@ -205,14 +192,14 @@ export class CurveTrade extends Trade {
          * { name: "_indices", type: "uint256[8]" },
          * { name: "_min_received", type: "uint256" }
          */
-        const methodSignatureName = 'exchange(int128,int128,uint256,uint256)'
+        const exchangeMethodSignature = 'exchange(int128,int128,uint256,uint256)'
         const args: (string | string[])[] = [
           tokenInIndex.toString(),
           tokenOutIndex.toString(),
           amountInBN.toString(),
           expectedAmountOut.toString()
         ]
-        const populatedTransaction = await curve3PoolContract.populateTransaction[methodSignatureName](...args, {
+        const populatedTransaction = await curve3PoolContract.populateTransaction[exchangeMethodSignature](...args, {
           value
         })
         // Assign the CurveTrade
@@ -223,59 +210,61 @@ export class CurveTrade extends Trade {
             : new TokenAmount(tokenOut, expectedAmountOut.toBigInt()),
           maximumSlippage,
           TradeType.EXACT_INPUT,
-          routerContract.address,
+          curve3PoolContract.address,
           populatedTransaction.data as string,
           value
         )
-      } else if (chainId == ChainId.ARBITRUM_ONE) {
-        /**
-         * Arbitrum WIP
-         */
-      } else if (chainId == ChainId.MAINNET) {
-        /**
-         * Ethereum WIP
-         */
-        const [routes, indices, expectedAmountOut] = await routerContract.get_exchange_routing(
-          currencyInAddress,
-          currencyOutAddress,
-          amountInBN.toString(),
-          {
-            from: ZERO_ADDRESS
-          }
+      }
+      // Arbitrum One
+      else if (chainId == ChainId.ARBITRUM_ONE) {
+        // Find all pools that the trade can go through
+        const routablePools = curvePools.filter(({ tokens }) =>
+          tokens.some(token => token.address.toLowerCase() === tokenIn.address.toLowerCase())
         )
-        /**
-         * Construct the call data from router method exchange
-         *
-         * exchange(int128 i, int128 j, uint256_dx, uint256 _min_dy)
-         * where:
-         * { name: "_amount", type: "uint256" },
-         * { name: "_route", type: "address[6]" },
-         * { name: "_indices", type: "uint256[8]" },
-         * { name: "_min_received", type: "uint256" }
-         */
-        let methodSignatureName = 'exchange(uint256,address[6],uint256[8],uint256)'
-        const args: (string | string[])[] = [
-          amountInBN.toString(),
-          routes,
-          indices.map((index: string) => index.toString()),
-          expectedAmountOut.toString()
-        ]
-
-        const populatedTransaction = await routerContract.populateTransaction[methodSignatureName](...args, {
-          value
-        })
-        // Return the CurveTrade
-        bestTrade = new CurveTrade(
-          currencyAmountIn,
-          Currency.isNative(currencyOut)
-            ? CurrencyAmount.nativeCurrency(expectedAmountOut.toBigInt(), chainId)
-            : new TokenAmount(tokenOut, expectedAmountOut.toBigInt()),
-          maximumSlippage,
-          TradeType.EXACT_INPUT,
-          routerContract.address,
-          populatedTransaction.data as string,
-          value
+        // For each pool, find the best outcome
+        // const trades = []
+        const bestTrades = await Promise.all(
+          routablePools.map(async pool => {
+            const poolContract = new Contract(pool.swapAddress, pool.abi, getProvider(chainId))
+            // Map token address to index
+            const tokenInIndex = pool.tokens.findIndex(({ symbol }) => symbol == tokenInSymbol)
+            const tokenOutIndex = pool.tokens.findIndex(({ symbol }) => symbol == tokenOutSymbol)
+            // Get expected output from the pool
+            const dyMethodSignature = pool.isMeta ? 'get_dy_underlying' : 'get_dy'
+            const expectedAmountOut = (await poolContract[dyMethodSignature](
+              tokenInIndex.toString(),
+              tokenOutIndex.toString(),
+              amountInBN.toString()
+            )) as BigNumber
+            // Construct the unsigned transaction
+            const exchangeMethodSignature = 'exchange_underlying(uint256,uint256,uint256,uint256)'
+            const args: (string | string[])[] = [
+              tokenInIndex.toString(),
+              tokenOutIndex.toString(),
+              amountInBN.toString(),
+              expectedAmountOut.toString()
+            ]
+            const populatedTransaction = await poolContract.populateTransaction[exchangeMethodSignature](...args, {
+              value
+            })
+            // Return the CurveTrade
+            return new CurveTrade(
+              currencyAmountIn,
+              Currency.isNative(currencyOut)
+                ? CurrencyAmount.nativeCurrency(expectedAmountOut.toBigInt(), chainId)
+                : new TokenAmount(tokenOut, expectedAmountOut.toBigInt()),
+              maximumSlippage,
+              TradeType.EXACT_INPUT,
+              poolContract.address,
+              populatedTransaction.data as string,
+              value
+            )
+          })
         )
+        bestTrade = bestTrades[0]
+      }
+      // Ethereum WIP
+      else if (chainId == ChainId.MAINNET) {
       }
     } catch (error) {
       console.error('could not fetch Curve trade', error)
