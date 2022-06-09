17b4bf31fa1d28f35a447a90924c4f1e6325d755
Adam Azad <adam@adamazad.com>
2022-06-09 08:56:10 +0100

    feat(#78): remove pair fee fetcher with cache layer

--- src/entities/trades/uniswap-v2/pairs/fees.ts
+++ src/entities/trades/uniswap-v2/pairs/fees.ts
@@ -3,51 +3,10 @@ import { Contract } from '@ethersproject/contracts'
 import { abi as UNISWAPR_PAIR_ABI } from '@swapr/core/build/IDXswapPair.json'
 
 import MULTICALL2_ABI from '../../../../abis/source/multicall2.json'
-import { ChainId, MULTICALL2_ADDRESS } from '../../../../constants'
+import { MULTICALL2_ADDRESS } from '../../../../constants'
 import { getProvider } from '../../utils'
-import { createCacheList } from '../cache'
 import type { Multicall2TryAggregateResult } from '../types'
 import { GetUniswapV2PairSwapFeeParams, UniswapV2PairSwapFee } from './types'
-
-/**
- * Cache for the UniswapV2 pair fee
- */
-export const uniswapV2PairFeeCache = createCacheList<{
-  expiresAt: number
-  data: number
-}>()
-
-/**
- * Fetches the pair fee from the contract or cache if it's already been fetched.
- * @param pairAddress the address of the pair
- * @param chainId the chain id of the pair
- * @param expiresIn the time in seconds until the cache expires
- * @returns the pair fee in basis points
- */
-export async function getUniswapV2PairSwapFeeWithCache(
-  pairAddress: string,
-  chainId: ChainId,
-  expiresIn = 3600
-): Promise<number> {
-  // Return the fee from the cache if it exists
-  const swapFeeFromCache = uniswapV2PairFeeCache[chainId].get(pairAddress)
-
-  // If the fee is in the cache, return it
-  if (swapFeeFromCache && swapFeeFromCache.expiresAt < Date.now()) {
-    return swapFeeFromCache.data
-  }
-
-  const pairContract = new Contract(pairAddress, UNISWAPR_PAIR_ABI, getProvider(chainId))
-  const swapFee = (await pairContract.swapFee()) as number
-
-  // Cache and return the fee
-  uniswapV2PairFeeCache[chainId].set(pairAddress, {
-    expiresAt: Date.now() + expiresIn * 1000,
-    data: swapFee,
-  })
-
-  return swapFee
-}
 
 /**
  * Given a list of UniswapV2 pair address, it fetches pair fee from the contract via multicall contract
