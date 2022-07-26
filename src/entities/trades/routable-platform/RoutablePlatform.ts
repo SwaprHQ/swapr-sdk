e42a685b5ad806edf533c643dd5e1fcd03070dd9
Adam Azad <adam@adamazad.com>
2022-07-26 13:26:16 +0100

    feat(#158): add CoWTrade with new CoW SDK and executionPriceWithoutFee
    - Deprecate GnosisProtocolTrade.
    - Remove GPv2 contracts package.
    - Remove extra files from old implementation

--- src/entities/trades/routable-platform/RoutablePlatform.ts
+++ src/entities/trades/routable-platform/RoutablePlatform.ts
@@ -7,7 +7,11 @@ import { BaseRoutablePlatform } from './BaseRoutablePlatform'
 export class RoutablePlatform extends BaseRoutablePlatform {
   public static readonly ZEROX = new RoutablePlatform([ChainId.MAINNET, ChainId.POLYGON], '0x')
   public static readonly CURVE = new RoutablePlatform([ChainId.MAINNET, ChainId.ARBITRUM_ONE, ChainId.XDAI], 'Curve')
+  /**
+   * @deprecated Use {@link RoutablePlatform.COW} instead.
+   */
   public static readonly GNOSIS_PROTOCOL = new RoutablePlatform([ChainId.MAINNET, ChainId.XDAI], 'CoW')
+  public static readonly COW = new RoutablePlatform([ChainId.MAINNET, ChainId.XDAI], 'CoW')
   public static readonly UNISWAP = new RoutablePlatform(
     [ChainId.MAINNET, ChainId.ARBITRUM_ONE, ChainId.POLYGON],
     'Uniswap'
