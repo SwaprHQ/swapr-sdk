57dd6927b2bb75e1fad25f6f1aa8c78011f40c19
Jorge Lopes <jorgedclopes@gmail.com>
2022-09-26 11:33:59 +0100

    Remove dist

--- dist/entities/trades/0x/utils.d.ts
+++
@@ -1,7 +0,0 @@
-import { Percent } from '../../fractions/percent';
-import { Platform } from '../../platforms-breakdown';
-import { ApiSource, DecodeStringFractionReturn } from './types';
-export declare const decodePlatformName: (apiName: string) => string;
-export declare const decodeStringFraction: (value: string) => DecodeStringFractionReturn;
-export declare const decodeStringToPercent: (value: string, isStringPercent?: boolean) => Percent;
-export declare const platformsFromSources: (sources: ApiSource[]) => Platform[];
