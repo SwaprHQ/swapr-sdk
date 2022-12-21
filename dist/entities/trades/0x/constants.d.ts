0b8cbc6b14b1781ca48a50e70b8be71ab5479c51
Violet <milanrasovic.f@gmail.com>
2022-12-21 13:54:20 +0100

    fix: Remove dist

--- dist/entities/trades/0x/constants.d.ts
+++
@@ -1,8 +0,0 @@
-import { ChainId } from '../../../constants/chains';
-/**
- * Platforms names
- */
-export declare const CODE_TO_PLATFORM_NAME: {
-    [apiName: string]: string;
-};
-export declare const ZEROX_API_URL: Record<ChainId, string>;
