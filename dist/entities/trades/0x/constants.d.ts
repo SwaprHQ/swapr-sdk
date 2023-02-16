31494b84f9dd86a9d5828bda4bb011b77d98d8fb
Violet <milanrasovic.f@gmail.com>
2023-02-16 16:32:53 +0100

    fix: Added dist so that it can be tested on the app

---
+++ dist/entities/trades/0x/constants.d.ts
@@ -0,0 +1,3 @@
+import { ChainId } from '../../../constants/chains';
+export declare const ZEROX_API_URL: Record<ChainId, string>;
+export declare const fee = "0";
---
+++ dist/index.d.ts
@@ -0,0 +1,8 @@
+import JSBI from 'jsbi';
+export { JSBI };
+export * from './abis';
+export * from './constants';
+export * from './entities';
+export * from './errors';
+export * from './fetcher';
+export { parseBigintIsh } from './utils';
