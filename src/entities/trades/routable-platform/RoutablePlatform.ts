8b14f19f3ebc879d38abe39d0d02a40e76cee1ab
Wixzi <wixzi@pm.me>
2023-03-26 18:50:48 +0200

    Add BSC Mainnet to Uniswap

--- src/entities/trades/routable-platform/RoutablePlatform.ts
+++ src/entities/trades/routable-platform/RoutablePlatform.ts
@@ -17,7 +17,7 @@ export class RoutablePlatform extends BaseRoutablePlatform {
   public static readonly GNOSIS_PROTOCOL = new RoutablePlatform([ChainId.MAINNET, ChainId.XDAI], 'CoW')
   public static readonly COW = new RoutablePlatform([ChainId.MAINNET, ChainId.XDAI], 'CoW')
   public static readonly UNISWAP = new RoutablePlatform(
-    [ChainId.MAINNET, ChainId.ARBITRUM_ONE, ChainId.POLYGON, ChainId.OPTIMISM_MAINNET],
+    [ChainId.MAINNET, ChainId.ARBITRUM_ONE, ChainId.POLYGON, ChainId.OPTIMISM_MAINNET, ChainId.BSC_MAINNET],
     'Uniswap'
   )
   public static readonly ONE_INCH = new RoutablePlatform(
