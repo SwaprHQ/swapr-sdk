b539b7538eebab5b732f335052b901f5a70ddc7a
Rorry <rodrigojali@gmail.com>
2023-05-15 12:14:59 -0300

    feat(SWA-30): add dist folder for testing purposes

---
+++ dist/entities/fractions/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const tslib_1 = require("tslib");
+tslib_1.__exportStar(require("./currencyAmount"), exports);
+tslib_1.__exportStar(require("./fraction"), exports);
+tslib_1.__exportStar(require("./percent"), exports);
+tslib_1.__exportStar(require("./price"), exports);
+tslib_1.__exportStar(require("./priced-token-amount"), exports);
+tslib_1.__exportStar(require("./tokenAmount"), exports);
+//# sourceMappingURL=index.js.map
---
+++ dist/entities/trades/0x/constants.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ZERO_OX_REFFERER_FEE = exports.ZEROX_API_URL = void 0;
+const chains_1 = require("../../../constants/chains");
+exports.ZEROX_API_URL = {
+    [chains_1.ChainId.MAINNET]: 'https://api.0x.org/',
+    [chains_1.ChainId.RINKEBY]: '',
+    [chains_1.ChainId.ARBITRUM_ONE]: 'https://arbitrum.api.0x.org/',
+    [chains_1.ChainId.ARBITRUM_RINKEBY]: '',
+    [chains_1.ChainId.ARBITRUM_GOERLI]: '',
+    [chains_1.ChainId.XDAI]: '',
+    [chains_1.ChainId.POLYGON]: 'https://polygon.api.0x.org/',
+    [chains_1.ChainId.GOERLI]: '',
+    [chains_1.ChainId.OPTIMISM_MAINNET]: 'https://optimism.api.0x.org/',
+    [chains_1.ChainId.OPTIMISM_GOERLI]: '',
+    [chains_1.ChainId.BSC_MAINNET]: 'https://bsc.api.0x.org/',
+    [chains_1.ChainId.BSC_TESTNET]: '',
+};
+//API DOCS TO REFRENCE THIS https://docs.0x.org/0x-api-swap/api-references/get-swap-v1-quote
+//buyTOkenPercetageFee value
+exports.ZERO_OX_REFFERER_FEE = '0'; //MIN-> 0. MAX-> 1 percent
+//# sourceMappingURL=constants.js.map
