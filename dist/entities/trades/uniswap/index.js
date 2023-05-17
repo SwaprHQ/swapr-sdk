9cf74326d622ae51bb686cc0f02999e5e258f406
Rorry <rodrigojali@gmail.com>
2023-05-17 11:04:19 -0300

    Revert "feat(SWA-30): add dist folder for testing purposes"
    This reverts commit 5600b3325c9e8e0f4aa815e29f8d601c535892a3.

--- dist/entities/trades/uniswap/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UniswapTrade = void 0;
-var Uniswap_1 = require("./Uniswap/Uniswap");
-Object.defineProperty(exports, "UniswapTrade", { enumerable: true, get: function () { return Uniswap_1.UniswapTrade; } });
-//# sourceMappingURL=index.js.map
--- dist/entities/trades/uniswap/utils/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { TradeType } from '../../../../constants';
-export * from './nativeCurrency';
-export declare function encodeRecipient(tradeType: TradeType, recipient: string, callData?: string): string | undefined;
