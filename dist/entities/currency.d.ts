cd72a0d7a9553de7e21d7229d69a80e6bec64c7f
Rorry <rodrigojali@gmail.com>
2023-06-01 13:44:36 -0300

    chore(SWA-15): update dist folder for testing purposes

--- dist/entities/currency.d.ts
+++ dist/entities/currency.d.ts
@@ -15,7 +15,6 @@ export declare class Currency {
      */
     static readonly ETHER: Currency;
     static readonly OPTIMISM_ETHER: Currency;
-    static readonly ZK_SYNC_ERA_ETHER: Currency;
     /**
      * Gnosis Chain native currency
      */
--- dist/entities/trades/0x/constants.d.ts
+++ dist/entities/trades/0x/constants.d.ts
@@ -1,6 +1,7 @@
 import { ChainId } from '../../../constants/chains';
 /**
- * For API endpoints reference, @see https://0x.org/docs/introduction/0x-cheat-sheet
+ * For API endpoints reference,
+ * @see https://0x.org/docs/introduction/0x-cheat-sheet
  */
 export declare const ZEROX_API_URL: Record<ChainId, string>;
 export declare const ZERO_OX_REFFERER_FEE = "0";
