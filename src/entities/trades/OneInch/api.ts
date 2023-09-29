f8b19c549d8f6857fc1e44f0959479d0109c2e21
Rorry <lil.rorry@gmail.com>
2023-09-29 10:23:35 -0300

    fix(SWA-98): Update 1inch  API config (#317)
    * fix(SWA-98): fix our code to fit latest 1inch API changes

--- src/entities/trades/OneInch/api.ts
+++ src/entities/trades/OneInch/api.ts
@@ -1,11 +1,27 @@
 import { ChainId } from '../../../constants'
 import { REFFERER_ADDRESS_CHAIN_MAPPING } from '../constants'
 
-const apiBaseUrl = (chainId: number) => 'https://api.1inch.io/v5.0/' + chainId
-
 export enum RequestType {
   QUOTE = '/quote',
   SWAP = '/swap',
+}
+
+enum ApiName {
+  FUSION = '/fusion',
+  SPOT_PRICE = '/price',
+  SWAP = '/swap',
+}
+
+enum SubApiName {
+  ORDERS = '/orders',
+  QUOTER = '/quoter',
+  RELAYER = '/relayer',
+}
+
+enum ApiVersion {
+  FUSION = '/v1.0',
+  SPOT_PRICE = '/v1.1',
+  SWAP = '/v5.2',
 }
 
 interface ApiRequestUrlParams {
@@ -14,9 +30,22 @@ interface ApiRequestUrlParams {
   chainId: ChainId
 }
 
-//0.1% fee here is link to api https://docs.1inch.io/docs/aggregation-protocol/api/swap-params
+interface ApiUrlConfig {
+  apiName: ApiName
+  apiVersion: ApiVersion
+  chainId: number
+  subApiName?: SubApiName | ''
+}
 
+/**
+ * @see https://portal.1inch.dev/documentation/swap/introduction
+ */
+
+const API_BASE_URL = process.env.REACT_APP_ONEINCH_BASE_API_URL ?? 'https://api.1inch.dev/'
 const ONE_INCH_REFFERER_FEE = '0' //MIN-> 0 MAX-> 3
+
+const getApiUrl = ({ apiName, apiVersion, chainId, subApiName = '' }: ApiUrlConfig) =>
+  `${API_BASE_URL}${apiName}${subApiName}${apiVersion}/${chainId}`
 
 export function generateApiRequestUrl({ methodName, queryParams, chainId }: ApiRequestUrlParams) {
   if (REFFERER_ADDRESS_CHAIN_MAPPING[chainId]) {
@@ -24,9 +53,14 @@ export function generateApiRequestUrl({ methodName, queryParams, chainId }: ApiR
     queryParams.fee = ONE_INCH_REFFERER_FEE
   }
 
-  return apiBaseUrl(chainId) + methodName + '?' + new URLSearchParams(queryParams).toString()
+  return (
+    getApiUrl({ apiName: ApiName.SWAP, apiVersion: ApiVersion.SWAP, chainId }) +
+    methodName +
+    '?' +
+    new URLSearchParams(queryParams).toString()
+  )
 }
 
 export function approveAddressUrl(chainId: ChainId) {
-  return apiBaseUrl(chainId) + '/approve/spender'
+  return getApiUrl({ apiName: ApiName.SWAP, apiVersion: ApiVersion.SWAP, chainId }) + '/approve/spender'
 }
