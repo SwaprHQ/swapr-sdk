676e7338052925e9245eb223319fb2ac47f2a032
Diogo Ferreira <diogomf@hey.com>
2024-01-02 16:24:46 +0000

    Small fixes based on comments

--- src/entities/trades/swapr-v3/contracts.ts
+++ src/entities/trades/swapr-v3/contracts.ts
@@ -2,15 +2,16 @@ import { Contract } from 'ethers'
 import { SWAPR_ALGEBRA_POOL_ABI, SWAPR_ALGEBRA_QUOTER_ABI, SWAPR_ALGEBRA_ROUTER_ABI } from './abi'
 import { getProvider } from '../utils'
 import { SWAPR_ALGEBRA_CONTRACTS } from './constants'
+import { ChainId } from '../../../constants'
 
 export function getPoolsContract(pool_address: string) {
-  return new Contract(pool_address, SWAPR_ALGEBRA_POOL_ABI, getProvider(100))
+  return new Contract(pool_address, SWAPR_ALGEBRA_POOL_ABI, getProvider(ChainId.GNOSIS))
 }
 
 export function getRouterContract() {
-  return new Contract(SWAPR_ALGEBRA_CONTRACTS.router, SWAPR_ALGEBRA_ROUTER_ABI, getProvider(100))
+  return new Contract(SWAPR_ALGEBRA_CONTRACTS.router, SWAPR_ALGEBRA_ROUTER_ABI, getProvider(ChainId.GNOSIS))
 }
 
 export function getQuoterContract() {
-  return new Contract(SWAPR_ALGEBRA_CONTRACTS.quoter, SWAPR_ALGEBRA_QUOTER_ABI, getProvider(100))
+  return new Contract(SWAPR_ALGEBRA_CONTRACTS.quoter, SWAPR_ALGEBRA_QUOTER_ABI, getProvider(ChainId.GNOSIS))
 }
