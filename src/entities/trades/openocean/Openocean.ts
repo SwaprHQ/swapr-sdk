75f653fe972a1f7757ff8dcd6fd2bf453bb4e413
Rorry <rodrigojali@gmail.com>
2024-03-13 11:53:44 -0300

    feat(SWA-146): make Openocean getQuote work. swapTransaction still pending

--- src/entities/trades/openocean/Openocean.ts
+++ src/entities/trades/openocean/Openocean.ts
@@ -2,10 +2,11 @@ import { Contract } from '@ethersproject/contracts'
 import { BaseProvider } from '@ethersproject/providers'
 import { UnsignedTransaction } from '@ethersproject/transactions'
 import { parseUnits } from '@ethersproject/units'
+import { validateAndParseAddress } from '@uniswap/sdk-core'
 import fetch from 'node-fetch'
 import invariant from 'tiny-invariant'
 
-import { _10000, ChainId, ONE, TradeType } from '../../../constants'
+import { ChainId, ONE, TradeType } from '../../../constants'
 import { Currency } from '../../currency'
 import { CurrencyAmount, Fraction, Percent, Price, TokenAmount } from '../../fractions'
 import { maximumSlippage as defaultMaximumSlippage } from '../constants'
@@ -15,6 +16,7 @@ import { RoutablePlatform } from '../routable-platform'
 import { getProvider, tryGetChainId, wrappedCurrency } from '../utils'
 import { OO_EXCHANGE_V2_ABI } from './abi'
 import { getApiChainCode, MainnetChainIds, OO_API_BASE_URL, OO_API_ENDPOINTS } from './api'
+import { OO_CONTRACT_ADDRESS_BY_CHAIN } from './constants'
 
 export interface OpenoceanQuoteTypes {
   amount: CurrencyAmount
@@ -117,18 +119,17 @@ export class OpenoceanTrade extends Trade {
 
       const res = await fetch(params.toString())
       const data = await res.json()
+      console.log('data: ', data)
 
       if (data && amount) {
-        const approveAddress = data.routeProcessorAddr
+        const approveAddress = data.routeProcessorAddrY
         const currencyAmountIn = Currency.isNative(currencyIn)
-          ? CurrencyAmount.nativeCurrency(data.routeProcessorArgs.amountIn, chainId)
-          : new TokenAmount(wrappedCurrency(currencyIn, chainId), data.routeProcessorArgs.amountIn)
+          ? CurrencyAmount.nativeCurrency(data.data.inAmount, chainId)
+          : new TokenAmount(wrappedCurrency(currencyIn, chainId), data.data.inAmount)
 
         const currencyAmountOut = Currency.isNative(currencyOut)
-          ? CurrencyAmount.nativeCurrency(data.routeProcessorArgs.amountOutMin, chainId)
-          : new TokenAmount(wrappedCurrency(currencyOut, chainId), data.routeProcessorArgs.amountOutMin)
-
-        const priceImpact = new Percent(Math.trunc(Math.abs(data.priceImpact) * 10000).toString(), _10000)
+          ? CurrencyAmount.nativeCurrency(data.data.outAmount, chainId)
+          : new TokenAmount(wrappedCurrency(currencyOut, chainId), data.data.outAmount)
 
         return new OpenoceanTrade({
           maximumSlippage,
@@ -137,7 +138,7 @@ export class OpenoceanTrade extends Trade {
           tradeType,
           chainId,
           approveAddress,
-          priceImpact,
+          priceImpact: new Percent('0', '100'),
         })
       }
     } catch (error) {
@@ -186,22 +187,75 @@ export class OpenoceanTrade extends Trade {
      * @see https://docs.openocean.finance/dev/aggregator-api-and-sdk/aggregator-api/best-practice
      */
 
-    const params = {
-      chain: this.chainId,
-      inTokenAddress: this.inputAmount.currency.address,
-      outTokenAddress: this.outputAmount.currency.address,
-      amount: this.inputAmount.raw.toString(),
-      gasPrice: options.recipient,
-      slippage: this.maximumSlippage,
+    const inToken = this.inputAmount.currency
+    const outToken = this.outputAmount.currency
+    const amount = this.inputAmount
+    const maximumSlippage = this.maximumSlippage
+    const recipient = validateAndParseAddress(options.recipient)
+    const isNativeIn = Currency.isNative(inToken)
+    const amountIn = `0x${this.maximumAmountIn().raw.toString(16)}`
+    const value = isNativeIn ? amountIn : undefined
+
+    let tradeEstimatedGas, tradeGasPrice, tradeData
+
+    try {
+      // Ensure that the currencies are present
+      invariant(inToken.address && outToken.address, `getQuote: Currency address is required`)
+      const API_CHAIN_CODE = getApiChainCode(this.chainId as MainnetChainIds)
+      const API_WITH_CHAIN_CODE = `${OO_API_BASE_URL}/${API_CHAIN_CODE}`
+
+      const gasResponse = await fetch(`${API_WITH_CHAIN_CODE}/${OO_API_ENDPOINTS.GET_GAS}`)
+
+      if (!gasResponse.ok) throw new Error(`OpenoceanTrade.getQuote: failed to get gasPrice`)
+
+      const gasData = await gasResponse.json()
+      const quoteGasPrice = gasData.without_decimals.standard
+
+      const params = new URL(`${API_WITH_CHAIN_CODE}/${OO_API_ENDPOINTS.SWAP_QUOTE}`)
+
+      params.searchParams.set(
+        'inTokenAddress',
+        `${Currency.isNative(inToken) ? '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE' : inToken.address}`,
+      )
+      params.searchParams.set(
+        'outTokenAddress',
+        `${Currency.isNative(outToken) ? '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE' : outToken.address}`,
+      )
+      params.searchParams.set('amount', `${parseUnits(amount.toSignificant(), amount.currency.decimals).toString()}`)
+      params.searchParams.set('gasPrice', this.chainId === ChainId.MAINNET ? quoteGasPrice.maxFeePerGas : quoteGasPrice)
+      params.searchParams.set(
+        'slippage',
+        `${new Fraction(maximumSlippage.numerator, maximumSlippage.denominator).toSignificant(1)}`,
+      )
+      params.searchParams.set('account', `${recipient}`)
+
+      const res = await fetch(params.toString())
+      const swapQuoteData = await res.json()
+
+      const { estimatedGas, data, gasPrice } = swapQuoteData
+
+      console.log('data: ', swapQuoteData)
+      console.log('gasData: ', gasData)
+      tradeEstimatedGas = estimatedGas
+      tradeGasPrice = gasPrice
+      tradeData = data
+    } catch (error) {
+      console.log('error: ', error)
+      console.error('Openocean.getQuote: Error fetching the trade:', error.message)
     }
 
-    const value = Currency.isNative(this.inputAmount.currency) ? params : undefined
+    const swapParams = {
+      from: recipient,
+      to: OO_CONTRACT_ADDRESS_BY_CHAIN[this.chainId as MainnetChainIds],
+      gas: tradeEstimatedGas,
+      gasPrice: tradeGasPrice,
+      data: tradeData,
+    }
 
-    return new Contract(this.approveAddress, OO_EXCHANGE_V2_ABI).populateTransaction['processRoute'](
-      { ...params },
-      {
-        value,
-      },
-    )
+    console.log('swapParams: ', swapParams)
+
+    return new Contract(this.approveAddress, OO_EXCHANGE_V2_ABI).populateTransaction['swap'](swapParams, {
+      value,
+    })
   }
 }
--- src/entities/trades/openocean/openocean.spec.ts
+++ src/entities/trades/openocean/openocean.spec.ts
@@ -2,279 +2,80 @@ import { parseUnits } from '@ethersproject/units'
 
 import { ChainId, TradeType } from '../../../constants'
 import { Percent, TokenAmount } from '../../fractions'
-import { WXDAI } from '../../token'
+import { ARB, SWPR, WBNB, WETH, WMATIC } from '../../token'
 import { USDC } from '../uniswap-v2'
 import { OpenoceanTrade } from './Openocean'
 
 const maximumSlippage = new Percent('3', '100')
-// const tokenGNOGnosis = new Token(
-//   ChainId.GNOSIS,
-//   '0x9C58BAcC331c9aa871AFD802DB6379a98e80CEdb',
-//   18,
-//   'GNO',
-//   'Gnosis token',
-// )
-
 const recipient = '0x0000000000000000000000000000000000000000'
 
-describe.only('Openocean', () => {
+const QUOTE_TESTS = [
+  { chainId: ChainId.ARBITRUM_ONE, chainName: 'Arbitrum One', quoteCurrency: ARB, quoteCurrencyName: 'ARB' },
+  { chainId: ChainId.BSC_MAINNET, chainName: 'Binance Smart Chain', quoteCurrency: WBNB, quoteCurrencyName: 'WBNB' },
+  { chainId: ChainId.GNOSIS, chainName: 'Gnosis', quoteCurrency: SWPR, quoteCurrencyName: 'SWPR' },
+  { chainId: ChainId.MAINNET, chainName: 'Ethereum', quoteCurrency: WETH, quoteCurrencyName: 'WETH' },
+  { chainId: ChainId.OPTIMISM_MAINNET, chainName: 'Optimism', quoteCurrency: WETH, quoteCurrencyName: 'WETH' },
+  { chainId: ChainId.POLYGON, chainName: 'Polygon', quoteCurrency: WMATIC, quoteCurrencyName: 'WMATIC' },
+  { chainId: ChainId.SCROLL_MAINNET, chainName: 'Sroll', quoteCurrency: WETH, quoteCurrencyName: 'WETH' },
+  { chainId: ChainId.ZK_SYNC_ERA_MAINNET, chainName: 'ZK Sync Era', quoteCurrency: WETH, quoteCurrencyName: 'WETH' },
+]
+
+const SWAP_TESTS = [
+  // { chainId: ChainId.ARBITRUM_ONE, chainName: 'Arbitrum One', quoteCurrency: ARB, quoteCurrencyName: 'ARB' },
+  // { chainId: ChainId.BSC_MAINNET, chainName: 'Binance Smart Chain', quoteCurrency: WBNB, quoteCurrencyName: 'WBNB' },
+  { chainId: ChainId.GNOSIS, chainName: 'Gnosis', quoteCurrency: SWPR, quoteCurrencyName: 'SWPR' },
+  // { chainId: ChainId.MAINNET, chainName: 'Ethereum', quoteCurrency: WETH, quoteCurrencyName: 'WETH' },
+  // { chainId: ChainId.OPTIMISM_MAINNET, chainName: 'Optimism', quoteCurrency: WETH, quoteCurrencyName: 'WETH' },
+  // { chainId: ChainId.POLYGON, chainName: 'Polygon', quoteCurrency: WMATIC, quoteCurrencyName: 'WMATIC' },
+  // { chainId: ChainId.SCROLL_MAINNET, chainName: 'Sroll', quoteCurrency: WETH, quoteCurrencyName: 'WETH' },
+  // { chainId: ChainId.ZK_SYNC_ERA_MAINNET, chainName: 'ZK Sync Era', quoteCurrency: WETH, quoteCurrencyName: 'WETH' },
+]
+
+describe('Openocean', () => {
   describe('Quote', () => {
-    test('should return a EXACT INPUT quote on Gnosis for USDC - WXDAI', async () => {
-      const currencyAmount = new TokenAmount(
-        USDC[ChainId.GNOSIS],
-        parseUnits('2879.59', USDC[ChainId.GNOSIS].decimals).toString(),
-      )
-      const trade = await OpenoceanTrade.getQuote({
-        amount: currencyAmount,
-        quoteCurrency: WXDAI[ChainId.GNOSIS],
-        maximumSlippage,
-        recipient,
-        tradeType: TradeType.EXACT_INPUT,
-      })
+    it.each(QUOTE_TESTS)(
+      'should return a EXACT INPUT quote on $chainName for USDC - $quoteCurrencyName',
+      async ({ chainId, quoteCurrency }) => {
+        const currencyAmount = new TokenAmount(USDC[chainId], parseUnits('1000', 18).toString())
+        const trade = await OpenoceanTrade.getQuote({
+          amount: currencyAmount,
+          quoteCurrency: quoteCurrency[chainId],
+          maximumSlippage,
+          recipient,
+          tradeType: TradeType.EXACT_INPUT,
+        })
 
-      expect(trade).toBeDefined()
-      expect(trade).not.toBeNull()
-      expect(trade?.chainId).toEqual(ChainId.GNOSIS)
-      expect(trade?.tradeType).toEqual(TradeType.EXACT_INPUT)
-      expect(trade?.outputAmount.currency.address).toBe(WXDAI[ChainId.GNOSIS].address)
-    })
-
-    // test('should return a EXACT INPUT quote on Gnosis for SWPR - WXDAI', async () => {
-    //   const currencyAmount = new TokenAmount(WXDAI[ChainId.GNOSIS], parseUnits('1000', 18).toString())
-    //   const trade = await OpenoceanTrade.getQuote({
-    //     amount: currencyAmount,
-    //     quoteCurrency: SWPR[ChainId.GNOSIS],
-    //     maximumSlippage,
-    //     recipient,
-    //     tradeType: TradeType.EXACT_INPUT,
-    //   })
-
-    //   expect(trade).toBeDefined()
-    //   expect(trade).not.toBeNull()
-    //   expect(trade?.chainId).toEqual(ChainId.GNOSIS)
-    //   expect(trade?.tradeType).toEqual(TradeType.EXACT_INPUT)
-    //   expect(trade?.outputAmount.currency.address).toBe(SWPR[ChainId.GNOSIS].address)
-    // })
-
-    // test('should return a EXACT INPUT quote on Gnosis for XDAI - WETH', async () => {
-    //   const currencyAmount = CurrencyAmount.nativeCurrency(parseEther('1000').toString(), ChainId.GNOSIS)
-    //   const trade = await OpenoceanTrade.getQuote({
-    //     amount: currencyAmount,
-    //     quoteCurrency: WETH[ChainId.GNOSIS],
-    //     maximumSlippage,
-    //     recipient,
-    //     tradeType: TradeType.EXACT_INPUT,
-    //   })
-
-    //   expect(trade).toBeDefined()
-    //   expect(trade).not.toBeNull()
-    //   expect(trade?.chainId).toEqual(ChainId.GNOSIS)
-    //   expect(trade?.tradeType).toEqual(TradeType.EXACT_INPUT)
-    //   expect(trade?.outputAmount.currency.address).toBe(WETH[ChainId.GNOSIS].address)
-    // })
-
-    // test('should return a EXACT INPUT quote on Gnosis for GNO - WETH', async () => {
-    //   const currencyAmount = new TokenAmount(tokenGNOGnosis, parseEther('1000000000').toString())
-    //   const trade = await OpenoceanTrade.getQuote({
-    //     amount: currencyAmount,
-    //     quoteCurrency: WETH[ChainId.GNOSIS],
-    //     maximumSlippage,
-    //     recipient,
-    //     tradeType: TradeType.EXACT_INPUT,
-    //   })
-
-    //   expect(trade).toBeDefined()
-    //   expect(trade?.chainId).toEqual(ChainId.GNOSIS)
-    //   expect(trade?.tradeType).toEqual(TradeType.EXACT_INPUT)
-    //   expect(trade?.outputAmount.currency.address).toBe(WETH[ChainId.GNOSIS].address)
-    // })
-
-    // test('should return a EXACT INPUT quote on Polygon for USDT - MATIC', async () => {
-    //   const currencyAmount = new TokenAmount(
-    //     USDT[ChainId.POLYGON],
-    //     parseUnits('1000000000', USDT[ChainId.POLYGON].decimals).toString(),
-    //   )
-    //   const currencyOut = Currency.getNative(ChainId.POLYGON)
-    //   const trade = await OpenoceanTrade.getQuote({
-    //     amount: currencyAmount,
-    //     quoteCurrency: currencyOut,
-    //     maximumSlippage,
-    //     recipient,
-    //     tradeType: TradeType.EXACT_INPUT,
-    //   })
-
-    //   expect(trade).toBeDefined()
-    //   expect(trade).not.toBeNull()
-    //   expect(trade?.chainId).toEqual(ChainId.POLYGON)
-    //   expect(trade?.tradeType).toEqual(TradeType.EXACT_INPUT)
-    //   expect(trade?.outputAmount.currency.address).toBe(currencyOut.address)
-    // })
-
-    // test('should return a EXACT INPUT quote on BSC for CAKE - BNB', async () => {
-    //   const currencyAmount = new TokenAmount(
-    //     CAKE[ChainId.BSC_MAINNET],
-    //     parseUnits('1000', CAKE[ChainId.BSC_MAINNET].decimals).toString(),
-    //   )
-    //   const currencyOut = Currency.getNative(ChainId.BSC_MAINNET)
-    //   const trade = await OpenoceanTrade.getQuote({
-    //     amount: currencyAmount,
-    //     quoteCurrency: currencyOut,
-    //     maximumSlippage,
-    //     recipient,
-    //     tradeType: TradeType.EXACT_INPUT,
-    //   })
-
-    //   expect(trade).toBeDefined()
-    //   expect(trade).not.toBeNull()
-    //   expect(trade?.chainId).toEqual(ChainId.BSC_MAINNET)
-    //   expect(trade?.tradeType).toEqual(TradeType.EXACT_INPUT)
-    //   expect(trade?.outputAmount.currency.address).toBe(currencyOut.address)
-    // })
-
-    // test('should return a EXACT INPUT quote on Arbitrum for ARB - ETH', async () => {
-    //   const currencyAmount = new TokenAmount(
-    //     ARB[ChainId.ARBITRUM_ONE],
-    //     parseUnits('1000', ARB[ChainId.ARBITRUM_ONE].decimals).toString(),
-    //   )
-    //   const currencyOut = Currency.getNative(ChainId.ARBITRUM_ONE)
-    //   const trade = await OpenoceanTrade.getQuote({
-    //     amount: currencyAmount,
-    //     quoteCurrency: currencyOut,
-    //     maximumSlippage,
-    //     recipient,
-    //     tradeType: TradeType.EXACT_INPUT,
-    //   })
-
-    //   expect(trade).toBeDefined()
-    //   expect(trade).not.toBeNull()
-    //   expect(trade?.chainId).toEqual(ChainId.ARBITRUM_ONE)
-    //   expect(trade?.tradeType).toEqual(TradeType.EXACT_INPUT)
-    //   expect(trade?.outputAmount.currency.address).toBe(currencyOut.address)
-    // })
-
-    // test('should return a EXACT INPUT quote on Optmism for DAI - ETH', async () => {
-    //   const currencyAmount = new TokenAmount(
-    //     DAI[ChainId.OPTIMISM_MAINNET],
-    //     parseUnits('1000', DAI[ChainId.OPTIMISM_MAINNET].decimals).toString(),
-    //   )
-    //   const currencyOut = Currency.getNative(ChainId.OPTIMISM_MAINNET)
-    //   const trade = await OpenoceanTrade.getQuote({
-    //     amount: currencyAmount,
-    //     quoteCurrency: currencyOut,
-    //     maximumSlippage,
-    //     recipient,
-    //     tradeType: TradeType.EXACT_INPUT,
-    //   })
-
-    //   expect(trade).toBeDefined()
-    //   expect(trade).not.toBeNull()
-    //   expect(trade?.chainId).toEqual(ChainId.OPTIMISM_MAINNET)
-    //   expect(trade?.tradeType).toEqual(TradeType.EXACT_INPUT)
-    //   expect(trade?.outputAmount.currency.address).toBe(currencyOut.address)
-    // })
-
-    // test('should return a EXACT INPUT quote on Mainnet for WETH - USDT', async () => {
-    //   const currencyAmount = new TokenAmount(
-    //     WETH[ChainId.MAINNET],
-    //     parseUnits('1000', WETH[ChainId.MAINNET].decimals).toString(),
-    //   )
-    //   const currencyOut = USDT[ChainId.MAINNET]
-    //   const trade = await OpenoceanTrade.getQuote({
-    //     amount: currencyAmount,
-    //     quoteCurrency: currencyOut,
-    //     maximumSlippage,
-    //     recipient,
-    //     tradeType: TradeType.EXACT_INPUT,
-    //   })
-
-    //   expect(trade).toBeDefined()
-    //   expect(trade).not.toBeNull()
-    //   expect(trade?.chainId).toEqual(ChainId.MAINNET)
-    //   expect(trade?.tradeType).toEqual(TradeType.EXACT_INPUT)
-    //   expect(trade?.outputAmount.currency.address).toBe(currencyOut.address)
-    // })
-
-    // test('should return a EXACT INPUT quote on Scroll for USDT - WETH', async () => {
-    //   const currencyAmount = new TokenAmount(
-    //     USDT[ChainId.SCROLL_MAINNET],
-    //     parseUnits('1000000000', USDT[ChainId.SCROLL_MAINNET].decimals).toString(),
-    //   )
-    //   const trade = await OpenoceanTrade.getQuote({
-    //     amount: currencyAmount,
-    //     quoteCurrency: WETH[ChainId.SCROLL_MAINNET],
-    //     maximumSlippage,
-    //     recipient,
-    //     tradeType: TradeType.EXACT_INPUT,
-    //   })
-
-    //   expect(trade).toBeDefined()
-    //   expect(trade?.chainId).toEqual(ChainId.SCROLL_MAINNET)
-    //   expect(trade?.tradeType).toEqual(TradeType.EXACT_INPUT)
-    //   expect(trade?.outputAmount.currency.address).toBe(WETH[ChainId.SCROLL_MAINNET].address)
-    // })
+        expect(trade).toBeDefined()
+        expect(trade).not.toBeNull()
+        expect(trade?.chainId).toEqual(chainId)
+        expect(trade?.tradeType).toEqual(TradeType.EXACT_INPUT)
+        expect(trade?.outputAmount.currency.address).toBe(quoteCurrency[chainId].address)
+      },
+    )
   })
 
-  // describe('Swap', () => {
-  //   test('should return a swap on Gnosis for USDC - WXDAI', async () => {
-  //     const currencyAmount = new TokenAmount(USDC[ChainId.GNOSIS], parseUnits('2', 6).toString())
+  describe('Swap', () => {
+    it.each(SWAP_TESTS)(
+      'should return a SWAP on $chainName for USDC - $quoteCurrencyName',
+      async ({ chainId, quoteCurrency }) => {
+        const currencyAmount = new TokenAmount(USDC[chainId], parseUnits('2', 6).toString())
 
-  //     const trade = await OpenoceanTrade.getQuote({
-  //       quoteCurrency: WXDAI[ChainId.GNOSIS],
-  //       amount: currencyAmount,
-  //       maximumSlippage,
-  //       recipient,
-  //       tradeType: TradeType.EXACT_INPUT,
-  //     })
+        const trade = await OpenoceanTrade.getQuote({
+          amount: currencyAmount,
+          quoteCurrency: quoteCurrency[chainId],
+          maximumSlippage,
+          recipient,
+          tradeType: TradeType.EXACT_INPUT,
+        })
 
-  //     const swapOptions = {
-  //       recipient,
-  //       account: recipient,
-  //     }
+        const swapOptions = {
+          recipient,
+          account: recipient,
+        }
 
-  //     const swap = await trade?.swapTransaction(swapOptions)
-  //     expect(swap !== undefined)
-  //   })
-
-  //   test('should return a swap on Gnosis with native token USDC - XDAI', async () => {
-  //     const currencyAmount = new TokenAmount(USDC[ChainId.GNOSIS], parseUnits('2', 6).toString())
-
-  //     const trade = await OpenoceanTrade.getQuote({
-  //       quoteCurrency: Currency.getNative(ChainId.GNOSIS),
-  //       amount: currencyAmount,
-  //       maximumSlippage,
-  //       recipient,
-  //       tradeType: TradeType.EXACT_INPUT,
-  //     })
-
-  //     const swapOptions = {
-  //       recipient,
-  //       account: recipient,
-  //     }
-
-  //     const swap = await trade?.swapTransaction(swapOptions)
-  //     expect(swap !== undefined)
-  //   })
-
-  //   test('should return a swap on Scroll for USDT - WETH', async () => {
-  //     const currencyAmount = new TokenAmount(
-  //       USDT[ChainId.SCROLL_MAINNET],
-  //       parseUnits('1000000000', USDT[ChainId.SCROLL_MAINNET].decimals).toString(),
-  //     )
-  //     const trade = await OpenoceanTrade.getQuote({
-  //       quoteCurrency: WETH[ChainId.SCROLL_MAINNET],
-  //       amount: currencyAmount,
-  //       maximumSlippage,
-  //       recipient,
-  //       tradeType: TradeType.EXACT_INPUT,
-  //     })
-
-  //     const swapOptions = {
-  //       recipient,
-  //       account: recipient,
-  //     }
-
-  //     const swap = await trade?.swapTransaction(swapOptions)
-  //     expect(swap !== undefined)
-  //   })
-  // })
+        const swap = await trade?.swapTransaction(swapOptions)
+        expect(swap !== undefined)
+      },
+    )
+  })
 })
