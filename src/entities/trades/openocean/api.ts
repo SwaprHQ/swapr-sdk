5a9bbcd6b7666185f1b4a9b68650b69ceee6f5fe
Rorry <rodrigojali@gmail.com>
2024-03-15 13:29:28 -0300

    feat(SWA-146): try new approach with OO API

--- src/entities/trades/openocean/api.ts
+++ src/entities/trades/openocean/api.ts
@@ -1,11 +1,14 @@
 import { ChainId } from '../../../constants'
 
-export const OO_API_BASE_URL = 'https://open-api.openocean.finance/v3'
+// export const OO_API_BASE_URL = 'https://open-api.openocean.finance/v3'
+export const OO_API_BASE_URL = 'https://ethapi.openocean.finance/v2'
 
 export enum OO_API_ENDPOINTS {
   GET_GAS = 'gasPrice',
+  GAS_PRICE = 'gas-price',
   QUOTE = 'quote',
   SWAP_QUOTE = 'swap_quote',
+  SWAP = 'swap',
 }
 
 export type TestChainIds =
@@ -22,19 +25,20 @@ export type MainnetChainIds = Exclude<ChainId, TestChainIds>
 /**
  * @see https://docs.openocean.finance/dev/supported-chains
  */
-const OO_API_CHAIN_CODE = {
-  [ChainId.ARBITRUM_ONE]: 'arbitrum',
-  [ChainId.BSC_MAINNET]: 'bsc',
-  [ChainId.GNOSIS]: 'xdai',
-  [ChainId.MAINNET]: 'eth',
-  [ChainId.OPTIMISM_MAINNET]: 'optimism',
-  [ChainId.POLYGON]: 'polygon',
-  [ChainId.SCROLL_MAINNET]: 'scroll',
-  [ChainId.ZK_SYNC_ERA_MAINNET]: 'zksync',
-}
+// const OO_API_CHAIN_CODE = {
+//   [ChainId.ARBITRUM_ONE]: 'arbitrum',
+//   [ChainId.BSC_MAINNET]: 'bsc',
+//   [ChainId.GNOSIS]: 'xdai',
+//   [ChainId.MAINNET]: 'eth',
+//   [ChainId.OPTIMISM_MAINNET]: 'optimism',
+//   [ChainId.POLYGON]: 'polygon',
+//   [ChainId.SCROLL_MAINNET]: 'scroll',
+//   [ChainId.ZK_SYNC_ERA_MAINNET]: 'zksync',
+// }
 
 export const getBaseUrlWithChainCode = (chainId: MainnetChainIds) => {
-  const API_CHAIN_CODE = OO_API_CHAIN_CODE[chainId]
+  // const API_CHAIN_CODE = OO_API_CHAIN_CODE[chainId]
+  return `${OO_API_BASE_URL}/${chainId}`
 
-  return `${OO_API_BASE_URL}/${API_CHAIN_CODE}`
+  // return `${OO_API_BASE_URL}/${API_CHAIN_CODE}`
 }
